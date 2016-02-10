0990c32bda4369c658437742b38c93466e3f40c9
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-02-10 20:18:58 +0100

    Working client

--- client/project.clj
+++ client/project.clj
@@ -1,6 +1,14 @@
 (defproject inlein/client "0.1.0-SNAPSHOT"
-  :description "Client program for Inlein"
+  :description "Inlein your dependencies."
   :url "https://github.com/hyPiRion/inlein"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [])
+  :source-paths []
+  :java-source-paths ["src"]
+  :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
+  :dependencies []
+  :main inlein.client.Main
+  :scm {:dir ".."}
+  :uberjar-name "inlein-%s.jar"
+  ;; to avoid complaints from Leiningen
+  :profiles {:uberjar {:aot :all}})
---
+++ client/src/inlein/client/Main.java
@@ -0,0 +1,7 @@
+package inlein.client;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
