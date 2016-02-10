538b0e82033e799ab6ff67b63101a6dd9e27fc2a
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-02-10 20:35:39 +0100

    Setup client tests

--- client/project.clj
+++ client/project.clj
@@ -3,12 +3,20 @@
   :url "https://github.com/hyPiRion/inlein"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies []
   :source-paths []
   :java-source-paths ["src"]
   :javac-options ["-target" "1.7" "-source" "1.7" "-Xlint:-options"]
-  :dependencies []
   :main inlein.client.Main
   :scm {:dir ".."}
+  :aliases {"test" "junit"
+            "javadoc" ["shell" "javadoc" "-d" "javadoc/inlein-${:version}"
+                       "-sourcepath" "src" "inlein.client"]}
+  :plugins [[lein-shell "0.5.0"]
+            [lein-junit "1.1.8"]]
   :uberjar-name "inlein-%s.jar"
   ;; to avoid complaints from Leiningen
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:java-source-paths ["test"]
+                   :junit ["test"]
+                   :dependencies [[junit/junit "4.11"]]}})
---
+++ client/test/inlein/client/DummyTest.java
@@ -0,0 +1,12 @@
+package inlein.client;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class DummyTest {
+    @Test
+    public void testJunitTest() throws Exception {
+        assertEquals(2+2, 4);
+        assertTrue("2+2 != 4 ???", 2+2 == 4);
+    }
+}
