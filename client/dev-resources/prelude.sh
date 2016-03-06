34cdc51ee2a206dae73074a18a857b4e66edd9a0
Jean Niklas L'orange <jeannikl@hypirion.com>
2016-03-06 22:13:07 +0100

    Hey, this actually seems to work!
    Fixes #1, probably (?)

--- client/dev-resources/prelude.sh
+++ client/dev-resources/prelude.sh
@@ -1,3 +1,3 @@
-#!/bin/sh
-
-exec java "-Xbootclasspath/a:$0" "-Dinlein.client.file=$0" inlein.client.Main "$@"
+:;exec java "-Xbootclasspath/a:$0" "-Dinlein.client.file=$0" inlein.client.Main "$@"
+@java -Xbootclasspath/a:"%~f0" -Dinlein.client.file="%~f0" inlein.client.Main %*
+@goto :eof
